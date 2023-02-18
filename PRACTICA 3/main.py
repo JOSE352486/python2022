ac6ed2b372fc7fe9dd19a2159994f02a20b71493
JOSE352486 <122219413+JOSE352486@users.noreply.github.com>
2023-02-17 19:19:39 -0500

    Create main.py

---
+++ PRACTICA 3/main.py
@@ -0,0 +1,14 @@
+print("DIGITE EL NOMBRE DE LA PREGUNTA A SELECCIONAR ")
+while True:
+ r=int((input()))
+ if r==1:
+  import practica
+ elif r==2:
+  import practica2
+ elif r==3:
+  import practica3
+ elif r==4:
+  import practica4
+  
+  
+
