fd10cb162e4f3fdf1f9cce6b29f9a1ce6497df52
JOSE352486 <122219413+JOSE352486@users.noreply.github.com>
2023-02-17 19:19:44 -0500

    Create pc4.py

---
+++ PRACTICA 3/pc4.py
@@ -0,0 +1,8 @@
+def recursiva(numero):
+    if numero == 1:
+        return 1
+    else: 
+        return numero + recursiva(numero -1)
+def division(numero1,num2):
+    return float(numero1/num2)
+
